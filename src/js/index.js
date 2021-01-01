699c2423f4d95d9f0e4d9be5f32cec8c967465d2
Edward <edwuwork@gmail.com>
2021-01-01 14:16:26 +0200

    Version 2.1.0
    Added some features (media and some useful tools)
    
    - Changelog
    -- Added media
    -- Made structure better
    -- Added SVG injction
    -- Added Jquery
    
    - Errors
    -- Watchpack Error (initial scan): Error: EACCES: permission denied (localhost needs to be opened by hand)

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,8 @@
-import '../styles/bootstrap.scss'
+// Styles
 import '../styles/style.sass'
+// Libraries
+var $ = require( "jquery" );
+import '@iconfu/svg-inject/dist/svg-inject'
+
+//Logo and svg
+SVGInject(document.querySelectorAll("img.injectable"))
